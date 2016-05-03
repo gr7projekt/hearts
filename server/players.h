92904476cd8d6b6db024234a2fd1b9abbe59d0d7
root <root@debian>
2016-05-03 04:41:22 -0500

    card.h och players.h (osäkert om dessa kompilerar eller ej)

--- server/players.h
+++ server/players.h
@@ -2,7 +2,7 @@
 //  players.h
 //  Test
 //
-//  Created by Johan Lipecki on 2016-04-15.
+//  Created by Johan Lipecki and Viggo Lundén on 2016-05-03.
 //  Copyright © 2016 Johan Lipecki. All rights reserved.
 //
 
@@ -10,5 +10,18 @@
 #define players_h
 
 #include <stdio.h>
+
+int player_waits_or_plays (int argc, char *argv[]);
+// Pekare eftersom att pthread_create() tar en pekare
+void *play_hand(void *parameters);
+
+struct player_parms;
+typedef struct player_parms{
+    int pos;
+    int id;
+    int score;
+    Card game_hand[13];
+    Card won_hand[52];
+}Player;
 
 #endif /* players_h */
