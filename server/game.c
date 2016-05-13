bf4778895d9c5322262aa904d361f161619cf16b
Johan Lipecki <lipecki@kth.se>
2016-05-13 10:46:10 +0200

    spelfunktioner tillagda

--- server/game.c
+++ server/game.c
@@ -2,13 +2,41 @@
 // Created by Johan Lipecki on 2016-05-12.
 //
 
+
+
 #include "game.h"
 
-int calculate_hand(char* card[]){
-    int i=0, score = 0;
-    for(;i<4;i++){
-        if (is_hearts(card[i])) score += 1;
-        else if (is_queen_of_diamonds(card)) score += 13;
+int calculate_trick(char* card[]){
+    int score = 0;
+    for(int i=0;i<4;i++){
+        if (is_hearts(*card[i])) score += 1;
+        else if (is_queen_of_spades(*card)) score += 13;
     }
     return score;
+}
+void update_round_score(int *round_score[],char *card[]){
+    for(int i=0;i<4;i++) *round_score[i] += *card[i];
+}
+
+int check_winner(char *card[],int player_pos_of_starting_hand) {
+    char hand_suit = card[player_pos_of_starting_hand][0];
+    char high_card[] = {NULL, NULL,'\0'};
+    char tmp_card[3];
+    int winner_pos=player_pos_of_starting_hand;
+    for (int i = 0;i < 4;i++) {
+        strcpy(tmp_card,*card[i]);
+        if (card[i][0] == hand_suit) if (card[i][1] > high_card[1]) {
+            strcpy(high_card,tmp_card);
+            winner_pos = i;
+        }
+    }
+    return winner_pos;
+}
+bool is_hearts(char suit) {
+    if ((suit == '2')) return true;
+    else return false;
+}
+bool is_queen_of_spades(char card[]){
+    if(strcmp(card,"0A")) return true;
+    else return false;
 }
--- server/game.h
+++ server/game.h
@@ -6,11 +6,13 @@
 #define SERVER_GAME_H
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
 
 //Takes an array of four cards and returns the score
-int calculate_hand(char* []);
+int calculate_trick(char* []);
 bool is_hearts(char);
-bool is_queen_of_diamonds(char[]);
+bool is_queen_of_spades(char[]);
 
 #endif SERVER_GAME_H
 
