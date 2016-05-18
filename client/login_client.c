1c789e06c76e31baa81acf2e6833126e366fffd4
Johan Lipecki <lipecki@kth.se>
2016-05-19 00:57:54 +0200

    ping pong

--- client/login_client.c
+++ client/login_client.c
@@ -20,37 +20,42 @@ int main(void)
     IPaddress ip;
     TCPsocket sd;
     char server[25] = "", newport[MAXLEN], SYN0[MAXLEN] = "hearts", SYN1[MAXLEN] = "port", log_string[40], pid[7];
-    int port, result, len, len2, fd;
+    int result, len, len2;
+    uint16_t port;
+    FILE *fd;
     
     sprintf(pid,".%d",getpid());
     strcpy(log_string, LOGIN_LOG);
-    strcat(log_string, pid);
-    fd = open(log_string,O_RDWR|O_CREAT,0640);
-    close(2);
-    dup(fd);
-    
+    //strcat(log_string, pid);
+    fd = fopen(log_string,"w+");
+    fprintf(fd,"open for business!\n");
+    fclose(fd);
+    fd = fopen(log_string,"a+");
+
     strcpy(server, IP_ADDRESS);
     port = PORT;
     
     if (SDLNet_ResolveHost(&ip, server, port) < 0)
     {
-        fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+        fprintf(fd, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
     
     if (!(sd = SDLNet_TCP_Open(&ip)))
     {
-        fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+        fprintf(fd, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
-    printf("socket open\n");
+    fd = fopen(log_string,"a");
+    fprintf(fd,"socket open\n");
+    printf("open\n");
     
     
     len = (int) strlen(SYN0)+1;
     
     if (SDLNet_TCP_Send(sd, SYN0,len) < len)
     {
-        fprintf(stderr, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+        fprintf(fd, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }
     printf("hearts sent\n");
@@ -58,32 +63,36 @@ int main(void)
     memset(SYN0, '\0', sizeof(SYN0));
     
     result=SDLNet_TCP_Recv(sd,SYN0,MAXLEN);
-    freopen(LOGIN_LOG,"w",stderr);
     printf("Recv: %d\n",result);
 
-    printf("%s\n",SYN0);
+    fprintf(fd,"%s\n",SYN0);
 
     if(strcmp(SYN0, EXPECTED_RESPONSE) == 0)
     {
         len2 = (int) strlen(SYN1)+1;
         if (SDLNet_TCP_Send(sd, SYN1,len2) < len2)
         {
-            fprintf(stderr, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+            printf( "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
             exit(EXIT_FAILURE);
         }
-        fprintf(stderr, "SDLNet_TCP_Sent: %s\n",SYN1);
+        printf("SDLNet_TCP_Sent: %s\n",SYN1);
         
         memset(SYN1, '\0', sizeof(SYN1));
         // Servern skickar "ENDOFTRANS" efter varje ACK
         // Klienten väntar på ett portnummer(40-50 k) och spelarposition[0:3]
         do {
             result=SDLNet_TCP_Recv(sd,SYN1,MAXLEN);
-            printf("Recv: %s ",SYN1);
-            system("xterm");
-            wait(0);
+            printf("Recv: %s \n",SYN1);
             sleep(3);
         } while (strcmp(SYN1, "account"));
-        
+        len2 = (int) strlen(USER_DATA) + 1;
+        while(!strcmp(SYN1, "account")) {
+            if (SDLNet_TCP_Send(sd, USER_DATA, len2) < len2) printf("%s",strerror(errno));
+            printf("Sent: %s\n", USER_DATA);
+            sleep(1);
+            printf("%d\n",SDLNet_TCP_Recv(sd,SYN1,MAXLEN));
+        }
+
         //sammanfoga strängen som startar spelklienten
         strcpy(newport, "./");
         strcat(newport, GAME_CLIENT);
@@ -92,10 +101,12 @@ int main(void)
         strcat(newport, SYN1);
         printf("commandline argument: %s \n", newport);
         execlp("/bin/sh","sh","-c",newport,NULL);
-        fprintf(stderr, "%s", strerror(errno));
+        fprintf(fd, "%s", strerror(errno));
         exit(EXIT_FAILURE);
     }
-    else fprintf(stderr, "Returned error string: %s\n", SYN0);
+
+    else fprintf(fd, "Returned error string: %s\n", SYN0);
+    fclose(fd);
     return -1;
 }
 
