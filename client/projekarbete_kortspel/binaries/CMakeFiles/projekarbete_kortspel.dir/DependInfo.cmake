d7f0a55c7ae32205fb54748996b664d1592bd494
Niclas Ragnar <nragnar@kth.se>
2016-05-18 11:00:42 +0200

    CMake & Makefiles
    MAC only?

---
+++ client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/Button.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/Button.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/cardPosition.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/cardPosition.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/loadCard.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/loadCard.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/loadMedia.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/loadMedia.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/logik.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/logik.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/mainClient.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/mainClient.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/moveCard.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/moveCard.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/openWeb.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/openWeb.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/position.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/position.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/rendering.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/rendering.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/resetTurn.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/resetTurn.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/send.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/send.c.o"
+  "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/window.c" "/Users/niclasragnar/Desktop/git/hearts/client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/SDL2.framework/Headers"
+  "/Library/Frameworks/SDL2_image.framework/Headers"
+  "/Library/Frameworks/SDL2_ttf.framework/Headers"
+  "/Library/Frameworks/SDL2_net.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ client/projekarbete_kortspel/binaries/CMakeFiles/projekarbete_kortspel.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekarbete_kortspel.dir/Button.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/cardPosition.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/loadCard.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/loadMedia.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/logik.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/mainClient.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/moveCard.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/openWeb.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/position.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/rendering.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/resetTurn.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/send.c.o"
+  "CMakeFiles/projekarbete_kortspel.dir/window.c.o"
+  "../projekarbete_kortspel.pdb"
+  "../projekarbete_kortspel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projekarbete_kortspel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
